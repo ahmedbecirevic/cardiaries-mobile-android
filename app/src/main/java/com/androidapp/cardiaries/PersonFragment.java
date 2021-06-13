7bb3c0f71e7d2eb1281d83b9ad0209cdaeb03d92
ahmedbecirevic <ahmed.becirevic@stu.ibu.edu.ba>
2021-06-13 09:50:06 +0200

    design improve, add log out feature

--- app/src/main/java/com/androidapp/cardiaries/PersonFragment.java
+++ app/src/main/java/com/androidapp/cardiaries/PersonFragment.java
@@ -1,18 +1,38 @@
 package com.androidapp.cardiaries;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class PersonFragment extends Fragment {
+    private Button logOutButton;
+    private Context context;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        context = container.getContext();
         return inflater.inflate(R.layout.fragment_person, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        logOutButton = view.findViewById(R.id.log_out_button);
+        logOutButton.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), Login.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        });
+    }
+
 }
