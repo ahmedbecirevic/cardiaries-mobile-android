80183aca6b2a1e7d6b81c33b583c03eede985df5
ahmedbecirevic <ahmed.becirevic@stu.ibu.edu.ba>
2021-06-13 16:49:46 +0200

    maps current location add

--- app/src/main/java/com/androidapp/cardiaries/MapViewFragment.java
+++ app/src/main/java/com/androidapp/cardiaries/MapViewFragment.java
@@ -1,15 +1,22 @@
 package com.androidapp.cardiaries;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -19,18 +26,22 @@ import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MapViewFragment extends Fragment {
     MapView mMapView;
     private GoogleMap googleMap;
+    Button share_location;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_dashboard, container, false);
-
-        mMapView = (MapView) rootView.findViewById(R.id.mapView);
+        share_location = rootView.findViewById(R.id.give_location_permission_btn);
+        mMapView = rootView.findViewById(R.id.mapView);
         mMapView.onCreate(savedInstanceState);
 
         mMapView.onResume(); // needed to get the map to display immediately
@@ -41,36 +52,69 @@ public class MapViewFragment extends Fragment {
             e.printStackTrace();
         }
 
-        mMapView.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(GoogleMap mMap) {
-                googleMap = mMap;
+        mMapView.getMapAsync(mMap -> {
+            googleMap = mMap;
 
-                // For showing a move to my location button
-                if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return;
+//            share_location.setOnClickListener(v -> {
+////                && ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+////                && shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION)
+//                if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//                    if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) ) {
+//                        Toast.makeText(getContext(), "this is additional info", Toast.LENGTH_SHORT).show();
+//                    } else {
+//                        ActivityCompat.requestPermissions(getActivity(),new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 25);
+//                        final LocationListener mLocationListener = new LocationListener() {
+//                            @Override
+//                            public void onLocationChanged(final Location location) {
+//                                mMap.clear();
+//                                LatLng myLocation = new LatLng(location.getLatitude(), location.getLongitude());
+//                                mMap.addMarker(new MarkerOptions().position(myLocation).title("Your current location"));
+//                                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 15.0f));
+//                            }
+//                        };
+//                    }
+//                }
+//            });
+
+            // For showing a move to my location button
+//                if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//                    // TODO: Consider calling
+//                    //    ActivityCompat#requestPermissions
+//                    // here to request the missing permissions, and then overriding
+//                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//                    //                                          int[] grantResults)
+//                    // to handle the case where the user grants the permission. See the documentation
+//                    // for ActivityCompat#requestPermissions for more details.
+//                    return;
+//                }
+//                googleMap.setMyLocationEnabled(true);
+
+            LatLng sydney = new LatLng(-34, 151);
+            Marker marker = googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
+            CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
+            googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+            share_location.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Marker currentMarker = marker;
+                    mMap.clear();
+                    if (currentMarker!=null) {
+                        currentMarker.remove();
+                        currentMarker=null;
+                    }
+                    LatLng myLocation = new LatLng(43.839999, 18.340268);
+                    mMap.addMarker(new MarkerOptions().position(myLocation).title("Your location!"));
+                    CameraPosition cameraPosition = new CameraPosition.Builder().target(myLocation).zoom(12).build();
+                    googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
                 }
-                googleMap.setMyLocationEnabled(true);
+            });
 
-                // For dropping a marker at a point on the Map
-                LatLng sydney = new LatLng(-34, 151);
-                googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
-
-                // For zooming automatically to the location of the marker
-                CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
-                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-            }
         });
 
         return rootView;
     }
+
 
     @Override
     public void onResume() {
@@ -95,4 +139,10 @@ public class MapViewFragment extends Fragment {
         super.onLowMemory();
         mMapView.onLowMemory();
     }
+
+
+//    public boolean isLocationEnabled () {
+//        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+//        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+//    }
 }
